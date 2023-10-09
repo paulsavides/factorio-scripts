55c37971bfc0f0ff76954a9840c222d40d5be26e
paulsavides <paulsavides@outlook.com>
2023-10-08 19:08:41 -0700

    fix systemctl name

--- scripts/restart-service.sh
+++ scripts/restart-service.sh
@@ -2,7 +2,7 @@
 
 source_root=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
-sytemctl stop factorio
+systemctl stop factorio
 systemctl daemon-reload
 systemctl reenable $source_root/../unit/factorio.service
 systemctl start factorio
