ac875317a310e36b4b1dd42109822d4f0b953807
paulsavides <paulsavides@outlook.com>
2024-10-17 22:50:48 -0700

    fix relative path to unit files

--- scripts/initial-provision.sh
+++ scripts/initial-provision.sh
@@ -18,7 +18,7 @@ log "running update in place script to download factorio..."
 log "creating initial save"
 su factorio -c "/opt/factorio/bin/x64/factorio --create /opt/factorio/saves/default.zip"
 
-systemctl enable $source_root/unit/factorio.service
-systemctl status $source_root/unit/factorio.service
+systemctl enable $source_root/../unit/factorio.service
+systemctl status $source_root/../unit/factorio.service
 
 
